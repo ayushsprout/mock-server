433aecffe0a770f4427c0f48617fb8664153f38d
ayushsprout <ayushagarwal@sprouts.ai>
2025-08-22 21:53:24 +0530

    added changes for console

--- src/services/mockService.ts
+++ src/services/mockService.ts
@@ -1,6 +1,7 @@
 export class MockService {
   static async getSproutResponse() {
     // simulate 2-second delay
+    console.log('Request received for Sprouts');
     await new Promise(resolve => setTimeout(resolve, 2000));
     return { name: "sprouts" };
   }
